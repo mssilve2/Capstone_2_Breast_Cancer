95eb53d79b5abbc8092966fad6533417243684cf
mssilve2 <31743324+mssilve2@users.noreply.github.com>
2022-04-19 11:32:46 -0400

    First commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Springboard Capstone 2 Breast Cancer Image Classification ',
+    author='Max Silverstein',
+    license='MIT',
+)
